289dd134f369b884a6248c3a9ad7b03100871695
abdichandra <chandra.mhmmd02@gmail.com>
2021-06-16 13:00:32 +0700

    Pratikum Pertemuan akhir

---
+++ src/id/ac/uniska/model/MataKuliah.java
@@ -0,0 +1,65 @@
+package id.ac.uniska.model;
+
+public class MataKuliah {
+
+    int IdMakul;
+    String namaMakul, singkatanMakul, namaDosen, kontakDosen;
+    boolean aktif;
+
+    public MataKuliah(int idMakul, String namaMakul, String singkatanMakul, String namaDosen, String kontakDosen, boolean aktif) {
+        IdMakul = idMakul;
+        this.namaMakul = namaMakul;
+        this.singkatanMakul = singkatanMakul;
+        this.namaDosen = namaDosen;
+        this.kontakDosen = kontakDosen;
+        this.aktif = aktif;
+    }
+
+    public int getIdMakul() {
+        return IdMakul;
+    }
+
+    public void setIdMakul(int idMakul) {
+        IdMakul = idMakul;
+    }
+
+    public String getNamaMakul() {
+        return namaMakul;
+    }
+
+    public void setNamaMakul(String namaMakul) {
+        this.namaMakul = namaMakul;
+    }
+
+    public String getSingkatanMakul() {
+        return singkatanMakul;
+    }
+
+    public void setSingkatanMakul(String singkatanMakul) {
+        this.singkatanMakul = singkatanMakul;
+    }
+
+    public String getNamaDosen() {
+        return namaDosen;
+    }
+
+    public void setNamaDosen(String namaDosen) {
+        this.namaDosen = namaDosen;
+    }
+
+    public String getKontakDosen() {
+        return kontakDosen;
+    }
+
+    public void setKontakDosen(String kontakDosen) {
+        this.kontakDosen = kontakDosen;
+    }
+
+    public boolean isAktif() {
+        return aktif;
+    }
+
+    public void setAktif(boolean aktif) {
+        this.aktif = aktif;
+    }
+}
